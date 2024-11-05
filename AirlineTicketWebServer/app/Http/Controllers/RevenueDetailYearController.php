f71814db65021ee64c4046c5b56b87438417ff49
Thuỷ <127014697+hoangxuanthuy@users.noreply.github.com>
2024-11-05 15:15:00 +0700

    Update and rename CT_BCDT_NamController.php to RevenueDetailYearController.php

--- AirlineTicketWebServer/app/Http/Controllers/RevenueDetailYearController.php
+++ AirlineTicketWebServer/app/Http/Controllers/RevenueDetailYearController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class CT_BCDT_NamController
+class RevenueDetailYearController
 {
     /**
      * Display a listing of the resource.
