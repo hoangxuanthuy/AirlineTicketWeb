62b3dfeff42d3b39a54eb76187abb66a413c2622
Thuỷ <127014697+hoangxuanthuy@users.noreply.github.com>
2024-11-05 15:44:21 +0700

    Update and rename ChuyenBay.php to Flight.php

--- AirlineTicketWebServer/app/Models/Flight.php
+++ AirlineTicketWebServer/app/Models/Flight.php
@@ -4,7 +4,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ChuyenBay extends Model
+class Flight  extends Model
 {
     use HasFactory;
 
