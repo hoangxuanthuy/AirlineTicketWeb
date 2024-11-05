09636fb9704bc864abcdc36e5634d62522b3a0af
Thuỷ <127014697+hoangxuanthuy@users.noreply.github.com>
2024-11-05 15:51:45 +0700

    Update and rename TrungGian.php to Intermediate.php

--- AirlineTicketWebServer/app/Models/Intermediate.php
+++ AirlineTicketWebServer/app/Models/Intermediate.php
@@ -4,7 +4,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class TrungGian extends Model
+class Intermediate extends Model
 {
     use HasFactory;
 
