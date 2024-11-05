b435914c985ac8e3fc08dace95430ae756b2abf3
Thuỷ <127014697+hoangxuanthuy@users.noreply.github.com>
2024-11-05 15:43:41 +0700

    Update and rename CT_BCDT_Thang.php to RevenueMonth.php

--- AirlineTicketWebServer/app/Models/RevenueMonth.php
+++ AirlineTicketWebServer/app/Models/RevenueMonth.php
@@ -4,7 +4,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class CT_BCDT_Thang extends Model
+class RevenueMonth extends Model
 {
     use HasFactory;
 
