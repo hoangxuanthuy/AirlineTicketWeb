71da3e0f516dff5ead9aa8becdccbb0902cffc39
Thuỷ <127014697+hoangxuanthuy@users.noreply.github.com>
2024-12-27 18:51:02 +0700

    Update SqlPromotion.php

--- Sever/app/Business/PromotionBiz/SqlPromotion.php
+++ Sever/app/Business/PromotionBiz/SqlPromotion.php
@@ -7,10 +7,42 @@ use Illuminate\Http\Request;
 class SqlPromotion
 {
     // Lấy tất cả chương trình khuyến mãi
-    public function getAllPromotions()
+    public function countPromotions(?string $search = null)
+{
+    $query = "SELECT COUNT(*) as total FROM Promotion WHERE IsDeleted = 0";
+
+    $bindings = [];
+
+    // Thêm điều kiện tìm kiếm nếu có
+    if (!empty($search)) {
+        $query .= " AND (promotion_name LIKE :search1 OR discount_percentage LIKE :search2 )";
+        $bindings['search1'] = '%' . $search . '%';
+        $bindings['search2'] = '%' . $search . '%';
+    }
+    // Thực thi query
+    $result = DB::select($query, $bindings);
+    return $result[0]->total ?? 0;
+}
+    public function getAllPromotions(int $limit = 10, int $offset = 0, ?string $search = null)
     {
-        $query = "SELECT * FROM Promotion WHERE IsDeleted = 0";
-        return DB::select($query);
+        $query = "SELECT *
+                FROM Promotion
+                WHERE IsDeleted = 0";
+
+        $bindings = [];
+
+        if (!empty($search)) {
+            $query .= " AND (promotion_name LIKE :search1 OR discount_percentage LIKE :search2 )";
+            $bindings['search1'] = '%' . $search . '%';
+            $bindings['search2'] = '%' . $search . '%';
+        }
+
+        // Thêm giới hạn và phân trang
+        $query .= " LIMIT :limit OFFSET :offset";
+        $bindings['limit'] = $limit;
+        $bindings['offset'] = $offset;
+
+        return DB::select($query, $bindings);
     }
 
     // Tạo chương trình khuyến mãi mới
