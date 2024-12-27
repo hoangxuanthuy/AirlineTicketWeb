e45e8973d9309b91ce0b4fc491713f7728a3977f
Thuỷ <127014697+hoangxuanthuy@users.noreply.github.com>
2024-12-27 18:41:05 +0700

    Update SqlSeatClass.php

--- Sever/app/Business/SeatClassBiz/SqlSeatClass.php
+++ Sever/app/Business/SeatClassBiz/SqlSeatClass.php
@@ -6,10 +6,42 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 class SqlSeatClass
 {
-    public function getAllSeatClasses()
+    public function countSeatClass(?string $search = null)
+{
+    $query = "SELECT COUNT(*) as total FROM SeatClass WHERE IsDeleted = 0";
+
+    $bindings = [];
+
+    // Thêm điều kiện tìm kiếm nếu có
+    if (!empty($search)) {
+        $query .= " AND (seat_class_name LIKE :search1 OR price_ratio LIKE :search2 )";
+        $bindings['search1'] = '%' . $search . '%';
+        $bindings['search2'] = '%' . $search . '%';
+    }
+    // Thực thi query
+    $result = DB::select($query, $bindings);
+    return $result[0]->total ?? 0;
+}
+    public function getAllSeatClass(int $limit = 10, int $offset = 0, ?string $search = null)
     {
-        $query = "SELECT * FROM SeatClass WHERE IsDeleted = 0";
-        return DB::select($query);
+        $query = "SELECT *
+                FROM SeatClass
+                WHERE IsDeleted = 0";
+
+        $bindings = [];
+
+        if (!empty($search)) {
+            $query .= " AND (seat_class_name LIKE :search1 OR price_ratio LIKE :search2 )";
+        $bindings['search1'] = '%' . $search . '%';
+        $bindings['search2'] = '%' . $search . '%';
+        }
+
+        // Thêm giới hạn và phân trang
+        $query .= " LIMIT :limit OFFSET :offset";
+        $bindings['limit'] = $limit;
+        $bindings['offset'] = $offset;
+
+        return DB::select($query, $bindings);
     }
 
     public function createSeatClass(array $data)
